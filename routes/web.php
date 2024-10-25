3609dd9191453e7ec06b77c89427b92c634c5e14
Iddrissmus <114881166+Iddrissmus@users.noreply.github.com>
2024-10-25 12:02:44 +0000

    added routes for amusement park tickets

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TicketController;
+use App\Http\Controllers\AmusementParkTicketController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +11,7 @@ Route::get('/', function () {
 
 Route::get('/ticket', [TicketController::class, 'create'])->name('tickets.create');
 Route::post('/ticket', [TicketController::class, 'store'])->name('tickets.store');
+
+
+Route::get('/amusement', [AmusementParkTicketController::class, 'create'])->name('amusement_park_tickets.create');
+Route::post('/amusement', [AmusementParkTicketController::class, 'store'])->name('amusement_park_tickets.store');
